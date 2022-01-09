7d30eb6863925f9cb44110fba7e73dfc60d4657a
Sameek Bhanja <96880389+Sameek-Bhanja@users.noreply.github.com>
2022-01-09 20:04:03 +0530

    SanitizeForm

---
+++ Login.php
@@ -0,0 +1,50 @@
+<?php
+   if(isset($_POST["submitButton"])) {
+   
+        $firstName = sanitizeFormString($_POST["firstName"]);
+
+   }
+   
+
+   function sanitizeFormString($inputText) {
+       $inputText = strip_tags($inputText);
+       //$inputText = str_replace (" ", "", $inputText)
+       $inputText = trim($inputText);
+       return $inputText;
+   }
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+       <title> Welcome to EMBERFLEX</title>
+       <link rel="stylesheet" type="text/css" href="assests/style/style.css"/>  
+    </head>
+    <body>
+       <div class="signInContainer">
+
+           <div class="column">
+
+               <div class="header">
+                   <img src="assests/images/EmberFlex.png" title="Logo" alt="Site Logo"/> 
+                   <h3>Login</h3>
+                   <span>to continue to EMBERFLEX</span>
+               </div>
+
+
+               <form method="POST">
+
+                    <input type="text" name="username" placeholder="Username" required>
+
+                    <input type="password" name="password" placeholder="Password" required>
+
+                    <input type="submit" name="submitButton" value="SUBMIT">
+
+               </form>
+                    <a href="register.php" class="signInMessage">Don't have account? Sign Up here!</a> 
+           </div>
+
+       </div>
+
+    </body>
+</html>
