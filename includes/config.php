eed93c97631fb72dc2f396cc00ceb5d4ae3a03ce
Sameek Bhanja <96880389+Sameek-Bhanja@users.noreply.github.com>
2022-01-10 16:56:23 +0530

    a

--- includes/config.php
+++ includes/config.php
@@ -5,10 +5,10 @@ session_start();
 date_default_timezone_set("America/Los_Angeles");
 
 try {
-    $con = new PDO("mysql:dbname=emaberflex;host=localhost", "root", "")
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING)
+    $con = new PDO("mysql:dbname=emberflex;host=localhost", "root", "");
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }
 catch (PDOException $e){
-    exit("Conection to DB Failed:" $e->getMessage())
+    exit("Conection to DB Failed:" . $e->getMessage());
 }
 ?>
