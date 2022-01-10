d76ee318124e65cd1cdd477e7fe1193903cfa382
Sameek Bhanja <96880389+Sameek-Bhanja@users.noreply.github.com>
2022-01-10 15:58:41 +0530

    gific dv connection

--- includes/config.php
+++ includes/config.php
@@ -1,3 +1,14 @@
 <?php
 ob_start(); // Turns on output buffering
+session_start();
+
+date_default_timezone_set("America/Los_Angeles");
+
+try {
+    $con = new PDO("mysql:dbname=emaberflex;host=localhost", "root", "")
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING)
+}
+catch (PDOException $e){
+    exit("Conection to DB Failed:" $e->getMessage())
+}
 ?>
