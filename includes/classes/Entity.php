acc88253e4538ebe0bd522039445b46043705b07
Sameek Bhanja <96880389+Sameek-Bhanja@users.noreply.github.com>
2022-01-12 13:32:04 +0530

    entity

---
+++ includes/classes/Entity.php
@@ -0,0 +1,22 @@
+<?php 
+class Entity {
+
+    private $con, $sqlData;
+
+    public function __contruct($con, $input) {
+        $this->con = $con
+
+        if(is_array($input)) {
+            $this->sqlData = $input
+        }
+        else {
+            $query = $this->con->prepare("SELECT * FROM entities WHERE id=:id");
+            $query->bindValue(":id", $input);
+            $query->execute();
+
+            $this->sqlData = $query->fetch(PDO::FETCH_ASSOC);
+        }
+    }
+
+}
+?>
