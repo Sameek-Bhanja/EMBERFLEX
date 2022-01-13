ee496434ec34e1cea29734aee9346949cdf435d8
Sameek Bhanja <96880389+Sameek-Bhanja@users.noreply.github.com>
2022-01-13 16:52:03 +0530

    efwe

--- includes/classes/CatagoryContainers.php
+++ includes/classes/CatagoryContainers.php
@@ -8,17 +8,24 @@ class CategoryContainers {
         $this->username = $username;
     }
 
-    public function showAllCayegories() {
+    public function showAllCategories() {
         $query = $this->con->prepare("SELECT * FROM categories");
         $query->execute();
 
         $html = "<div class='previewCategories'>";
 
         while($row = $query->fetch(PDO::FETCH_ASSOC)) {
-            $html .= $row["name"];
+            $html .= $this->getCategoryHtml($row, null, true, true);
         }
 
         return $html . "</div>";
+    }
+
+    private function getCategoryHtml($sqlData, $title, $tvShows, $movies) {
+        $categoryId = $sqlData["id"];
+        $title = $title == null ? $sqlData["name"] : $title;
+
+        return $title . "<br>";
     }
 
 }
--- index.php
+++ index.php
@@ -5,5 +5,5 @@ $preview = new PreviewProvider($con, $userLoggedIn);
 echo $preview->createPreviewVideo(null);
 
 $containers = new CategoryContainers($con, $userLoggedIn);
-echo $containers->showAllCayegories();
+echo $containers->showAllCategories();
 ?>
