cf841bec5209ba2fc66efae35b5039b60e48cd5f
Sameek Bhanja <96880389+Sameek-Bhanja@users.noreply.github.com>
2022-01-13 16:18:20 +0530

    commit big alwyas

---
+++ includes/classes/CatagoryContainers.php
@@ -0,0 +1,25 @@
+<?php 
+class CategoryContainers {
+
+    private $con, $username;
+
+    public function __construct($con, $username) {
+        $this->con = $con;
+        $this->username = $username;
+    }
+
+    public function showAllCayegories() {
+        $query = $this->con->prepare("SELECT * FROM categories");
+        $query->execute();
+
+        $html = "<div class='previewCategories'>";
+
+        while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            $html .= $row["name"];
+        }
+
+        return $html . "</div>";
+    }
+
+}
+?>
