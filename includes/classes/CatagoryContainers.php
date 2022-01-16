7b0ec2e6577da516016aa67c263582aa75cf4c8e
Sameek Bhanja <96880389+Sameek-Bhanja@users.noreply.github.com>
2022-01-16 20:07:08 +0530

    gvbsvs

--- includes/classes/CatagoryContainers.php
+++ includes/classes/CatagoryContainers.php
@@ -46,7 +46,15 @@ class CategoryContainers {
         }
         
 
-        return $entitiesHtml . "<br>";
+        return "<div class='category'>
+                    <a href='category.php?id=$categoryId'>
+                        <h3>$title</h3>
+                    </a>
+
+                    <div class='entities'>
+                        $entitiesHtml
+                    </div>
+                </div>";
     }
 
 }
