7db65876634794b226ce22b480547335d2eaae28
Sameek Bhanja <96880389+Sameek-Bhanja@users.noreply.github.com>
2022-01-30 16:57:28 +0530

    jfkfuft

--- home.php
+++ home.php
@@ -6,10 +6,7 @@ $hideNav = true;
        <title> Welcome to EMBERFLEX</title>
        <link rel="stylesheet" type="text/css" href="assests/style/style.css"/>
     </head>
-    <div class="wrapper">
-            <!--<div class="homelogoContainer">-->
+    <div class="homewrapper noScroll">
                 <img class="HomeLogoImage"  src="assests/images/EmberFlex.png" alt="Site logo">
-            <!--</div>-->
-            <div class="homeButtomHtml">
-                <a href="index.php" class="homelink"><input type="submit" name="homeButton" value="Go To Gallary" href="index.php"></a> 
-            </div>
+            <h3 class="homeH3">Welcome To EMBERFLEX</h3>
+                <a href="index.php" class="homelink"><input type="submit" name="homeButton" class="homeButton" value="Go To Gallary" href="index.php"></a> 
